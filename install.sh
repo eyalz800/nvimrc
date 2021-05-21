fd1deaf2e1cb26d881aee191f79f15e241e9129a
eyalz800 <eyalz800@gmail.com>
2021-05-21 04:52:31 +0300

    Enhanced install script.

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ sudo DEBIAN_FRONTEND=noninteractive apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y neovim
 
 cp init.vim ~/.config/nvim/init.vim
+nvim -E -s -u ~/.config/nvim/init.vim +'!python3 -m pip install --user --upgrade pynvim' +qa
