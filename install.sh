5146a908a43a0d1e26c963f23cc5798ec237234c
eyalz800 <eyalz800@gmail.com>
2021-05-25 05:01:16 +0300

    Use unstable.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:neovim-ppa/stable
+sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:neovim-ppa/unstable
 sudo DEBIAN_FRONTEND=noninteractive apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y neovim
 
