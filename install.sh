a97e2c5d76abf341c69711d5a461510a4eb65a0e
amiramitai <amiramitai@users.noreply.github.com>
2021-06-30 01:11:14 +0300

    Added mac flow

--- install.sh
+++ install.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:neovim-ppa/unstable
-sudo DEBIAN_FRONTEND=noninteractive apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install -y neovim
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    sudo DEBIAN_FRONTEND=noninteractive add-apt-repository -y ppa:neovim-ppa/unstable
+    sudo DEBIAN_FRONTEND=noninteractive apt update
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y neovim
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    brew install nvim
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 
 cp init.vim ~/.config/nvim/init.vim
 nvim -E -s -u ~/.config/nvim/init.vim +'!python3 -m pip install --user --upgrade pynvim' +qa
